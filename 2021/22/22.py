dda0136c62554329f2db59762a3ef3e64822b258
JFevil <43685685+JFevil@users.noreply.github.com>
2024-05-31 23:18:58 +0200

    Add files via upload

--- 2021/22/22.py
+++ 2021/22/22.py
@@ -1,8 +1,5 @@
 from itertools import product
 
-with open('input.txt','r') as f:
-	texte=f.read()
-
 def nombre(temp):
 	if temp[0]=='-':
 		return -nombre(temp[1:])
@@ -10,43 +7,68 @@ def nombre(temp):
 		return int(temp)
 	return temp
 
-t=[]
-ligne=[]
-temp=''
-for i in texte:
-	if i=='\n':
-		ligne.append(nombre(temp))
-		temp=''
-		t.append(ligne)
-		ligne=[]
-	elif i=='=' or i=='x' or i=='y' or i=='z' or i=='.' or i==',' or i==' ':
-		if temp!='':
-			ligne.append(nombre(temp))
-		temp=''
-	else:
-		temp+=i
-
-def valide_ligne(ligne):
-	if abs(ligne[1])<=50:
-		return True
-	return False
+with open('input.txt','r') as f:
+	texte=f.read()
+	
+data = []
+for ligne in texte.splitlines():
+	state, temp = ligne.split(" ")
+	x_temp, y_temp, z_temp = temp.split(",")
+	x0, x1 = map(int, x_temp[2:].split(".."))
+	y0, y1 = map(int, y_temp[2:].split(".."))
+	z0, z1 = map(int, z_temp[2:].split(".."))
+	data.append((state=="on", x0, x1, y0, y1, z0, z1))
 
 d=dict()
-for ligne in t:
-	if valide_ligne(ligne):
-		x=[i for i in range(ligne[1],ligne[2]+1)]
-		y=[i for i in range(ligne[3],ligne[4]+1)]
-		z=[i for i in range(ligne[5],ligne[6]+1)]
-		if ligne[0]=='on':
+for cuboid in data:
+	if abs(cuboid[1]) <= 50:
+		x=[i for i in range(cuboid[1],cuboid[2]+1)]
+		y=[i for i in range(cuboid[3],cuboid[4]+1)]
+		z=[i for i in range(cuboid[5],cuboid[6]+1)]
+		if cuboid[0]:
 			for i in product(x,y,z):
 				d[i]=1
 		else:
 			for i in product(x,y,z):
 				d[i]=0
-
+				
 res=0
 for i in d:
 	if d[i]==1:
 		res+=1
 
-print(res)
+print("Partie 1 :",res)
+
+def intersection(a, b):
+	x0 = max(a[1], b[1])
+	x1 = min(a[2], b[2])
+	y0 = max(a[3], b[3])
+	y1 = min(a[4], b[4])
+	z0 = max(a[5], b[5])
+	z1 = min(a[6], b[6])
+	if x0 <= x1 and y0 <= y1 and z0 <= z1:
+		return (x0, x1, y0, y1, z0, z1)
+
+def volume(cuboid):
+	state, x0, x1, y0, y1, z0, z1 = cuboid
+	signe = 1 if state else -1
+	return signe * (x1 - x0 + 1) * (y1 - y0 + 1) * (z1 - z0 + 1)
+
+cuboids = []
+for new in data:
+	update = []
+	for cuboid in cuboids:
+		inter = intersection(cuboid, new)
+		if inter:
+			update.append((not cuboid[0],) + inter)
+
+	if new[0]:
+		update.append(new)
+
+	cuboids.extend(update)
+
+res = 0
+for i in cuboids:
+	res += volume(i)
+
+print("Partie 2 :",res)
