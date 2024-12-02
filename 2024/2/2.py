f8b78ce7ba70c7f6f94c66efa4e93c3708a89d8e
JFevil <flechejeremy@gmail.com>
2024-12-02 06:38:54 +0100

    day 2 of 2024

---
+++ 2024/2/2.py
@@ -0,0 +1,20 @@
+with open("input.txt") as f:
+	texte = f.read()
+
+L = [list(map(int,i.split(" "))) for i in texte.strip().splitlines()]
+res1 = 0
+res2 = 0
+for i in L:
+	if all([-4<i[j]-i[j-1]<0 for j in range(1,len(i))]) or all([0<i[j]-i[j-1]<4 for j in range(1,len(i))]):
+		res1 += 1
+	else:
+		for j in range(len(i)):
+			temp = [k for k in i]
+			temp.pop(j)
+			if all([-4<temp[j]-temp[j-1]<0 for j in range(1,len(temp))]) or all([0<temp[j]-temp[j-1]<4 for j in range(1,len(temp))]):
+				res2 += 1
+				break
+
+
+print("Partie 1 :", res1)
+print("Partie 1 :", res1+res2)
