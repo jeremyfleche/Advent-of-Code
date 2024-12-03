8493fb3dfd102b3c032c9a3ec2723a87e195c050
JFevil <flechejeremy@gmail.com>
2024-12-03 07:06:02 +0100

    day 3 of 2024

--- 2021/24/24.py
+++ 2021/24/24.py
@@ -3,15 +3,12 @@ with open("input.txt") as f:
 
 L = [i.split() for i in texte]
 
-def iemeChiffre(n, i):
-	return n//(10**i)%10
-
-def algo(MONAD):
+def instructions(MONAD):
 	i = 0
 	d = {'w':0, 'x':0, 'y':0, 'z':0}
 	for line in L:
 		if line[0] == "inp":
-			d[line[1]] = iemeChiffre(MONAD, i)
+			d[line[1]] = MONAD[i]
 			i += 1
 		elif line[0] == "add":
 			b = d[line[2]] if line[2] in d else int(line[2])
@@ -33,12 +30,22 @@ def algo(MONAD):
 			d[line[1]] = 1 if d[line[1]] == (d[line[2]] if line[2] in d else int(line[2])) else 0
 	return d['z']
 
+def algo(MONAD):
+	print(len(cache))
+	if MONAD in cache:
+		return cache[MONAD], MONAD
+	if len(MONAD) == 14:
+		z = instructions(MONAD)
+		cache[MONAD] = z==0
+		return cache[MONAD], MONAD
+	for i in range(9, 0, -1):
+		new_MONAD = MONAD + (i,)
+		temp, M = algo(new_MONAD)
+		if temp:
+			cache[MONAD] = True 
+			return True, MONAD
+	cache[MONAD] = False
+	return False, MONAD
+
 cache = {}
-n = (10**14)-12
-lc = 0
-while True:
-	MONAD -= 1
-	for i in range(14):
-		if iemeChiffre(MONAD, i) == 0:
-			MONAD -= 10**i
-	return MONAD
+print(algo(tuple()))
