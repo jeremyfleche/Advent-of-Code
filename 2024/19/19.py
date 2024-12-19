0c47154f50c8c1eb42423ce7153d03c95813a0cf
Jérémy Flèche <flechejeremy@gmail.com>
2024-12-19 06:26:08 +0100

    day 19 of 2024

---
+++ 2024/19/19.py
@@ -0,0 +1,53 @@
+import time
+START_TIME = time.time()
+
+with open("input.txt") as f:
+	texte = f.read().strip()
+
+def isPossible(towel, patterns):
+	Q = [towel]
+	cache = set()
+	while Q:
+		remain = Q.pop()
+		if remain == "":
+			return True
+		if remain in cache:
+			continue
+		cache.add(remain)
+		for p in patterns:
+			n = len(p)
+			if n <= len(remain):
+				if p == remain[:n]:
+					Q.append(remain[n:])
+	return False
+
+def part2(towels, patterns):
+	cache = {}
+	def possibility(towel):
+		if towel in cache:
+			return cache[towel]
+		if towel == "":
+			return 1
+		res = 0
+		for p in patterns:
+			n = len(p)
+			if n <= len(towel):
+				if p == towel[:n]:
+					res += possibility(towel[n:])
+		cache[towel] = res
+		return res
+	res = 0
+	for i in towels:
+		res += possibility(i)
+	return res
+
+patterns = texte.split("\n\n")[0].split(", ")
+towels = texte.split("\n\n")[1].splitlines()
+
+res1 = sum([isPossible(towel, patterns) for towel in towels])
+print("Partie 1 :", res1)
+
+res2 = part2(towels, patterns)
+print("Partie 2 :", res2)
+
+print(f"[{int((time.time()-START_TIME)*1000)}ms]")
