7580773ca836349b6e5ee82af54fd549c7cebd38
Jérémy Flèche <flechejeremy@gmail.com>
2025-12-08 10:57:03 +0100

    Amélioration du jour 8 avec la création d'une structure Union-Find

--- 2025/8/8.py
+++ 2025/8/8.py
@@ -1,57 +1,44 @@
 import time
-from aoc import *
+from mymodules.aoc import *
 from collections import deque
 import os
+from mymodules.structures import UnionFind
+
 os.chdir(os.path.dirname(__file__))
 
 START_TIME = time.time()
 
 texte = read_input("input")
 
-L = [tuple(map(int,line)) for line in matrix(texte, ",")]
+lines = mapped_matrix(texte, ",")
 
 def distance(b1, b2):
 	a,b,c = b1
 	d,e,f = b2
 	return (abs(a-d)**2+abs(b-e)**2+abs(c-f)**2)**(1/2)
 
-d = {}
-for i in range(len(L)):
-	for j in range(i+1,len(L)):
-		d[(i,j)] = distance(L[i], L[j])
+distances = {}
+for i in range(len(lines)):
+	for j in range(i+1,len(lines)):
+		distances[(i,j)] = distance(lines[i], lines[j])
 
-sorted_distance = sorted(d, key=lambda kv: d[kv])
+sorted_by_distance = sorted(distances, key=lambda x: distances[x])
 
-def solve1():
-	circuits = []
-	for i in range(1000):
-		a,b = sorted_distance[i]
-		new_c = {a,b}
-		new_circuits = [c for c in circuits]
-		for c in circuits:
-			if new_c & c != set():
-				new_c |= c
-				new_circuits.remove(c)
-		circuits = new_circuits + [new_c]
+def solve():
+	UF = UnionFind()
+	wire = 0
+	while UF.count != 1 or UF.total_elements != len(lines):
+		w1, w2 = sorted_by_distance[wire]
+		UF.union(w1, w2)
+		wire += 1
+		if wire == 1000:
+			sizes = list(sorted(UF.size.values()))
+			res1 = sizes[-1]*sizes[-2]*sizes[-3]
+	return res1, lines[w1][0]*lines[w2][0]
+			
 
-	temp = list(sorted([len(c) for c in circuits]))
-	return temp[-1]*temp[-2]*temp[-3]
+res1, res2 = solve()
+print("Partie 1 :", res1)
+print("Partie 2 :", res2)
 
-def solve2():
-	i = 0
-	circuits = []
-	while len(circuits) != 1 or (len(circuits) == 1 and len(circuits[0]) != len(L)):
-		a,b = sorted_distance[i]
-		new_c = {a,b}
-		new_circuits = [i for i in circuits]
-		for c in circuits:
-			if new_c & c != set():
-				new_c |= c
-				new_circuits.remove(c)
-		circuits = new_circuits + [new_c]
-		i += 1
-	return L[a][0]*L[b][0]
-
-print("Partie 1 :", solve1())
-print("Partie 2 :", solve2())
 print(f"[{int((time.time()-START_TIME)*1000)}ms]")
